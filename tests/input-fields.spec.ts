cacf6ae43b2c20a3d57ba4530a86c3a3739a83d3
Habib Mgadmi <habib.mgadmi@hotmail.com>
2026-08-30 22:15:43 +0200

    complete practice assignment

---
+++ tests/input-fields.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach( async({page}) => {
+  await page.goto('/')
+})
+
+test('Home page is opened and Welcome message is displayed_1', async ({page}) => {
+  await expect(page.locator('.title')).toHaveText('Welcome to Petclinic')
+});
+test('Home page is opened and Welcome message is displayed_2', async ({page}) => {
+  await expect(page.locator('.title')).toHaveText('Welcome to Petclinic')
+});
+test('Home page is opened and Welcome message is displayed_3', async ({page}) => {
+  await expect(page.locator('.title')).toHaveText('Welcome to Petclinic')
+});
