fc272cf9335cbaa01851159f220a98369c626037
Habib Mgadmi <habib.mgadmi@hotmail.com>
2026-09-04 16:38:43 +0200

    first draft of assignment

--- tests/input-fields.spec.ts
+++ tests/input-fields.spec.ts
@@ -4,6 +4,29 @@ test.beforeEach( async({page}) => {
   await page.goto('/')
 })
 
-test('Home page is opened and Welcome message is displayed1', async ({page}) => {
-  await expect(page.locator('.title')).toHaveText('Welcome to Petclinic')
+test('PET TYPES menu', async ({page}) => {
+
+  await page.getByRole('link').getByText('PET TYPES').click()//1
+  await expect(page.getByRole('heading', { name: 'Pet Types' })).toBeVisible()//2
+  await page.getByRole('button', { name: 'Edit' }).first().click()//3
+  //4
+ await page.getByRole('textbox').clear()
+ await page.waitForTimeout(500)
+  await page.getByRole('textbox').fill('rabbit')
+  await page.getByRole('button', { name: 'Update' }).click()//5
+  //6
+
+  await page.getByRole('button', { name: 'Edit' }).first().click()
+  await page.getByRole('textbox').clear()
+  await page.waitForTimeout(500)
+  await page.getByRole('textbox').fill('cat')
+  await page.getByRole('button', { name: 'Update' }).click()
+  
+
+
+
+
+
+
+  
 });
